e8b843d918b98d6dba1553ce6f328f549617a2f8
amedipiran <robertopiranamedi@gmail.com>
2024-02-01 16:02:31 +0100

    lagt till bookcontext

---
+++ Data/BookContext.cs
@@ -0,0 +1,10 @@
+using Moment3_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Moment3_2.Data {
+public class BookContext : DbContext {
+    public BookContext(DbContextOptions<BookContext> options) : base(options);
+}
+
+}
---
+++ Models/Borrower.cs
@@ -0,0 +1,9 @@
+namespace Moment3_2.Models {
+    public class Borrower {
+        //Properties
+        public int Id {get; set;}
+
+        public string Name {get; set;}
+
+    }
+}
